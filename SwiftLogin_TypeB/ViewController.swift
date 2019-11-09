05221f6749134f99d8f51e951376a2b9347cf6cd
JuneK <atotalks@gmail.com>
2019-11-10 02:31:21 +0900

    first commit

---
+++ SwiftLogin_TypeB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftLogin_TypeB
+//
+//  Created by JuneK on 2019/11/10.
+//  Copyright © 2019 JuneK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var pwTextField: UITextField!
+    @IBOutlet weak var forgotTextField: UIImageView!
+    @IBOutlet weak var actionLogin: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
